851ea8be02f0ef7d4ab1b17afcf04e0b91cb69e6
Chubasic <joebereza@gmail.com>
2019-02-22 12:30:34 +0200

    view Main page

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -3,8 +3,10 @@ import './Footer.scss';
 
 export const Footer = () => {
   return (
-    <footer>
-        Footer(WIP)
+    <footer className='pt-5 pb-5'>
+      <div className='d-flex flex-row justify-content-center'>
+        <span>© 2019 GoAdventures.</span>
+      </div>
     </footer>
   );
 };
