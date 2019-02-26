d0e95c4c21c839204198c400420e16e2695fdc36
Artem Korniychuk <artem17thomas@gmail.com>
2019-02-26 11:57:55 +0200

    added new sign in method

--- server/goadventures/src/main/java/io/softserve/goadventures/auth/models/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package io.softserve.goadventures.auth.models;
-
-public class SecurityConstants {
-    public static final String SECRET = "SecretKey";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-    public  static final String TOKEN_PREFIX = "Bearer";
-}
