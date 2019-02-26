de2aa3fa1c64b921095a6eefe907671b8e10c358
Artem Korniychuk <artem17thomas@gmail.com>
2019-02-26 12:43:20 +0200

    completed sign in task: *added check for user status

---
+++ server/goadventures/src/main/java/io/softserve/goadventures/GoadventuresApplication.java
@@ -0,0 +1,13 @@
+package io.softserve.goadventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoadventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoadventuresApplication.class, args);
+	}
+
+}
---
+++ server/goadventures/src/main/java/io/softserve/goadventures/user/service/UserService.java
@@ -0,0 +1,32 @@
+package io.softserve.goadventures.user.service;
+
+import io.softserve.goadventures.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import io.softserve.goadventures.user.model.User;
+
+@Service
+public class UserService {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public Optional<User> getUserById(int id){
+     return userRepository.findById(id);
+  }
+
+  public User getUserByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
+
+  public void updateUser(User user) {
+    userRepository.save(user);
+  }
+
+}
