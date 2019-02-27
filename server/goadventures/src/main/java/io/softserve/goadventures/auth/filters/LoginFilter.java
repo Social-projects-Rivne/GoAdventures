21ada1cab70e64214a5549d67daeadcbfeeaa6fe
Luxobscr <luxobscr@gmail.com>
2019-02-27 14:10:44 +0200

    Updated LoginFilter to return Http Status

--- server/goadventures/src/main/java/io/softserve/goadventures/auth/filters/LoginFilter.java
+++ server/goadventures/src/main/java/io/softserve/goadventures/auth/filters/LoginFilter.java
@@ -2,12 +2,14 @@ package io.softserve.goadventures.auth.filters;
 
 import io.softserve.goadventures.auth.service.JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.springframework.http.HttpStatus;
 
     //@WebFilter("/reg/")
     @Component
@@ -18,29 +20,28 @@ import java.io.IOException;
         @Override
         public void destroy() {}
 
-        @Override
+
         public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterchain)
                 throws IOException, ServletException {
-
             HttpServletRequest request = (HttpServletRequest) req;
             HttpServletResponse response = (HttpServletResponse) res;
 
-            String loginURI = request.getContextPath() + "/auth/signin";
             String token = request.getHeader("Authorization");
-            System.out.println("In filter " + token);
             if (token != null) {
                 if (jwtService.parseToken(token) != null) {
+                    setOk(response);
                     filterchain.doFilter(request, response);
                 } else {
-                    response.sendRedirect(loginURI);
+                    setUnauthorized(response);
                 }
             } else {
-                response.sendRedirect(loginURI);
+                setUnauthorized(response);
             }
         }
 
             /*
             //HttpSession session = request.getSession(false);
+            String loginURI = request.getContextPath() + "/auth/signin";
             //boolean loggedIn = session != null && session.getAttribute("user") != null;
             //boolean loginRequest = request.getRequestURI().equals(loginURI);
             if (loggedIn || loginRequest) {
@@ -50,6 +51,18 @@ import java.io.IOException;
             }
             //filterchain.doFilter(request, response);
         }*/
+
+        public HttpServletResponse setOk(ServletResponse response) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(200);
+            return httpResponse;
+        }
+
+        public HttpServletResponse setUnauthorized(ServletResponse response) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.setStatus(401);
+            return httpResponse;
+        }
 
         @Override
         public void init(FilterConfig filterconfig) throws ServletException {}
