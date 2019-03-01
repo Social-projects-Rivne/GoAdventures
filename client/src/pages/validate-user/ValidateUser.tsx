5d8352cd29a0a4c0ef201bebb5ab2ae37938998d
Chubasic <joebereza@gmail.com>
2019-03-01 11:55:07 +0200

    Validate user page

--- client/src/pages/validate-user/ValidateUser.tsx
+++ client/src/pages/validate-user/ValidateUser.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function ValidateUser() {
+const ValidateUser = (props: any) => {
   return (
     <div>
-
     </div>
   );
-}
+};
+
+
+export default ValidateUser;
