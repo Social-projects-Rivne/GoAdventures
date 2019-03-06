ca0ef3367608ef46cd0c4f2944396329d2a5be19
Chubasic <joebereza@gmail.com>
2019-03-06 04:14:49 +0200

    Api methods

--- server/goadventures/src/main/java/io/softserve/goadventures/user/service/UserNotFoundException.java
+++ server/goadventures/src/main/java/io/softserve/goadventures/user/service/UserNotFoundException.java
@@ -1,4 +1,12 @@
 package io.softserve.goadventures.user.service;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User Not Found")
+public class UserNotFoundException extends Exception {
+  private static final long serialVersionUID = 200L;
+    public UserNotFoundException(String message){
+      super(message);
+  }
 }
