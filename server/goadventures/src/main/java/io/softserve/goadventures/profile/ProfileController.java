a463dee103a362c42b4ae8e5839823aec0083ec3
PseudoCoder228 <PseudoCoder228>
2019-03-07 12:13:46 +0200

    added edit profile method api

--- server/goadventures/src/main/java/io/softserve/goadventures/profile/ProfileController.java
+++ server/goadventures/src/main/java/io/softserve/goadventures/profile/ProfileController.java
@@ -1,15 +1,23 @@
 package io.softserve.goadventures.profile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.softserve.goadventures.auth.service.JWTService;
 import io.softserve.goadventures.user.model.User;
 import io.softserve.goadventures.user.repository.UserRepository;
+import io.softserve.goadventures.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+@CrossOrigin
 @RestController
 @RequestMapping("profile")
 public class ProfileController {
@@ -17,9 +25,13 @@ public class ProfileController {
 
     private final Logger logger = LoggerFactory.getLogger(ProfileController.class);
 
+    private final JWTService jwtService;
+    private final UserService userService;
+
     @Autowired
-    public ProfileController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public ProfileController(JWTService jwtService, UserService userService) {
+        this.jwtService = jwtService;
+        this.userService = userService;
     }
 
     @GetMapping("{userName}")
@@ -34,4 +46,36 @@ public class ProfileController {
 
         return profile;
     }
+
+
+    @GetMapping(path = "/edit-profile", produces = {MediaType.APPLICATION_JSON_VALUE} )
+    public void EditProfileData(@RequestHeader(value="Authorization") String authorizationHeader, @RequestBody User changeThisUser
+    )  {
+        String token = authorizationHeader;
+        logger.info(authorizationHeader);
+
+        User user = userService.getUserByEmail(jwtService.parseToken(token));
+        if(user!=null){
+            logger.info("Old data " + user.getEmail() + " " + user.getUsername());
+            user.setUsername(changeThisUser.getUsername());
+            user.setEmail(changeThisUser.getEmail());
+            userService.updateUser(user);
+
+
+            logger.info("new data "+ user.getEmail() + " " + user.getUsername());
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
