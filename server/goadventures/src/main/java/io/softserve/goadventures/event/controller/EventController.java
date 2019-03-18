b483e3fe458a483ab350268fa78916e97d405679
Chubasic <joebereza@gmail.com>
2019-03-18 12:40:22 +0200

    Fixed pagination

--- server/goadventures/src/main/java/io/softserve/goadventures/event/controller/EventController.java
+++ server/goadventures/src/main/java/io/softserve/goadventures/event/controller/EventController.java
@@ -28,99 +28,97 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 @RequestMapping("event")
 public class EventController {
-    private Logger logger = LoggerFactory.getLogger(io.softserve.goadventures.event.controller.EventController.class);
-    private final EventService eventService;
-    private final EventRepository eventRepository;
-    private final CategoryRepository categoryRepository;
-    private final GalleryRepository galleryRepository;
+  private Logger logger = LoggerFactory.getLogger(io.softserve.goadventures.event.controller.EventController.class);
+  private final EventService eventService;
+  private final EventRepository eventRepository;
+  private final CategoryRepository categoryRepository;
+  private final GalleryRepository galleryRepository;
 
-    @Autowired
-    public EventController(EventService eventService, EventRepository eventRepository,
-                           CategoryRepository categoryRepository, GalleryRepository galleryRepository) {
-        this.eventService = eventService;
-        this.eventRepository = eventRepository;
-        this.categoryRepository = categoryRepository;
-        this.galleryRepository = galleryRepository;
-    }
+  @Autowired
+  public EventController(EventService eventService, EventRepository eventRepository,
+                         CategoryRepository categoryRepository, GalleryRepository galleryRepository) {
+    this.eventService = eventService;
+    this.eventRepository = eventRepository;
+    this.categoryRepository = categoryRepository;
+    this.galleryRepository = galleryRepository;
+  }
 
-    @PostMapping("/create/{categoryId}")
-    public ResponseEntity<String> createEvent(@PathVariable(value = "categoryId") String categoryId, @RequestBody Event event) {
-        Category category = categoryRepository.findByCategoryName(categoryId);
-        event.setCategory(category);
-        eventService.addEvent(event);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        // event.setStatusId(EventStatus.CREATED.getEventStatus());
-        // eventService.addEvent(event);
+  @PostMapping("/create/{categoryId}")
+  public ResponseEntity<String> createEvent(@PathVariable(value = "categoryId") String categoryId, @RequestBody Event event) {
+    Category category = categoryRepository.findByCategoryName(categoryId);
+    event.setCategory(category);
+    eventService.addEvent(event);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    // event.setStatusId(EventStatus.CREATED.getEventStatus());
+    // eventService.addEvent(event);
 
-        return ResponseEntity.ok().headers(httpHeaders).body("Event created");
-    }
+    return ResponseEntity.ok().headers(httpHeaders).body("Event created");
+  }
 
-    @PostMapping("/category")
-    public ResponseEntity<String> createCategory(@RequestBody Category category) {
-        category.setEvents(null);
-        categoryRepository.save(category);
+  @PostMapping("/category")
+  public ResponseEntity<String> createCategory(@RequestBody Category category) {
+    category.setEvents(null);
+    categoryRepository.save(category);
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        return ResponseEntity.ok().headers(httpHeaders).body("Category created");
-    }
+    HttpHeaders httpHeaders = new HttpHeaders();
+    return ResponseEntity.ok().headers(httpHeaders).body("Category created");
+  }
 
-    @PostMapping("/gallery/{eventId}")
-    public ResponseEntity<String> createGallery(@PathVariable(value = "eventId") int eventId,
-                                                @RequestBody Gallery gallery) {
-        Event event = eventRepository.findById(eventId);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        gallery.setEventId(event);
-        galleryRepository.save(gallery);
-        return ResponseEntity.ok().headers(httpHeaders).body("gallery created");
-    }
+  @PostMapping("/gallery/{eventId}")
+  public ResponseEntity<String> createGallery(@PathVariable(value = "eventId") int eventId,
+                                              @RequestBody Gallery gallery) {
+    Event event = eventRepository.findById(eventId);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    gallery.setEventId(event);
+    galleryRepository.save(gallery);
+    return ResponseEntity.ok().headers(httpHeaders).body("gallery created");
+  }
 
-    @GetMapping("/all")
-    public ResponseEntity<?> getAllEvents(@PageableDefault(size = 15, sort = "id") Pageable eventPageable) {
-        Page<Event> eventsPage = eventService.getAllEvents(eventPageable);
-        System.out.println(eventsPage.hasNext());
-//        if (eventsPage.hasNext()) {
+  @GetMapping("/all")
+  public ResponseEntity<?> getAllEvents(@PageableDefault(size = 15, sort = "id") Pageable eventPageable) {
+    Page<Event> eventsPage = eventService.getAllEvents(eventPageable);
+    if(eventsPage != null) {
+      int nextPageNum = eventsPage.getNumber() + 1;
+      UriComponents uriComponentsBuilder = UriComponentsBuilder.newInstance()
+              .path("/event/all/")
+              .query("page={keyword}")
+              .buildAndExpand(nextPageNum);
+      HttpHeaders httpHeaders = new HttpHeaders();
+      httpHeaders.set("nextPage", uriComponentsBuilder.toString());
+      return new ResponseEntity<Slice<Event>>(eventsPage,
+              httpHeaders,
+              HttpStatus.OK);
+         } else {
+            // TODO: wr1 3r c
+            return  ResponseEntity.badRequest().body("End of pages");
+        }
+  }
 
-            int nextPageNum = eventsPage.getNumber() + 1;
-            UriComponents uriComponentsBuilder = UriComponentsBuilder.newInstance()
-                    .path("/event/all/")
-                    .query("page={keyword}")
-                    .buildAndExpand(nextPageNum);
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.set("nextPage", uriComponentsBuilder.toString());
-            return new ResponseEntity<Slice<Event>>(eventsPage,
-                     httpHeaders,
-                    HttpStatus.OK);
-//         } else {
-//            // TODO: wr1 3r c
-//            return  ResponseEntity.badRequest().body("End of pages");
-//        }
-    }
+  @GetMapping("/allCategory")
+  public Iterable<Category> getAllCategory() {
+    return categoryRepository.findAll();
+  }
 
-    @GetMapping("/allCategory")
-    public Iterable<Category> getAllCategory() {
-        return categoryRepository.findAll();
-    }
+  @GetMapping("/all/{location}")
+  public Iterable<EventDTO> getAllEvents(@PathVariable(value = "location") String location) {
+    return eventService.getEventsByLocation(location);
+  }
 
-    @GetMapping("/all/{location}")
-    public Iterable<EventDTO> getAllEvents(@PathVariable(value = "location") String location) {
-        return eventService.getEventsByLocation(location);
-    }
+  @GetMapping("/category/{categoryId}")
+  public Page<Event> getAllEventsByCategoryId(@PathVariable(value = "categoryId") int eventId, Pageable pageable) {
+    return eventRepository.findByCategoryId(eventId, pageable);
+  }
 
-    @GetMapping("/category/{categoryId}")
-    public Page<Event> getAllEventsByCategoryId(@PathVariable(value = "categoryId") int eventId, Pageable pageable) {
-        return eventRepository.findByCategoryId(eventId, pageable);
-    }
+  @GetMapping("/gallery/{eventId}")
+  public Iterable<Gallery> getAllGalleryByEventId(@PathVariable(value = "eventId") int eventId,
+                                                  Pageable pageable) {
+    Event event = eventRepository.findById(eventId);
+    return galleryRepository.findByEventId(event.getId());
+  }
 
-    @GetMapping("/gallery/{eventId}")
-    public Iterable<Gallery> getAllGalleryByEventId(@PathVariable(value = "eventId") int eventId,
-                                                    Pageable pageable) {
-        Event event = eventRepository.findById(eventId);
-        return galleryRepository.findByEventId(event.getId());
-    }
-
-    @GetMapping("/{eventId}")
-    public Event getEvent(@PathVariable(value = "eventId") int eventId) {
-        return eventService.getEventById(eventId);
-    }
+  @GetMapping("/{eventId}")
+  public Event getEvent(@PathVariable(value = "eventId") int eventId) {
+    return eventService.getEventById(eventId);
+  }
 
 }
