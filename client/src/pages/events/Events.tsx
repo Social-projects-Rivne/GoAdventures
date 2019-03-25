74e8bf723be44e8592c257586ddfa38d5237f118
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-03-25 14:57:09 +0200

    Added btn for addEvent

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
+
+
+import { DropDown } from '../../components/';
+import { AxiosResponse } from 'axios';
 import { getEventList } from '../../api/event.service';
 import { EventsListBuild } from '../../components/eventsListBuild/EventsListBuild';
 import { EventDto } from '../../interfaces/Event.dto';
+import { Ng2FloatBtnModule } from 'ng2-float-btn'
+import { AddEventBtn } from '../../components/addEventBtn/AddEventBtn';
+
+
+
 
 interface EventState {
   events: EventDto[];
-  pageSettings: {
-    isLast: true | false
-    nextPage: string | null
-  };
 }
 
 export class Events extends Component<EventDto, EventState> {
@@ -23,62 +27,37 @@ export class Events extends Component<EventDto, EventState> {
           id: 0,
           location: '',
           startDate: '',
-          topic: ''
+          topic: '',
+
+
         }
-      ],
-      pageSettings: {
-        isLast: false,
-        nextPage: ''
-      }
+      ]
     };
-    this.fetchEvents.bind(this);
   }
+
+
+
+
 
   public componentDidMount() {
-    this.fetchEvents();
+    const b = getEventList();
+    console.debug(b);
+    // this.setState({ events: [...response.data] });
   }
-
-  public async fetchEvents(): Promise<void> {
-    const response = await getEventList(this.state.pageSettings.nextPage);
-    this.setState({
-      events: [...response.content],
-      pageSettings: {
-        isLast: response.last,
-        nextPage: !!sessionStorage.getItem('nextpage')
-          ? sessionStorage.getItem('nextpage')
-          : '/event/all'
-      }
-    });
-  }
-
   public render() {
     return (
       <div className='container-fluid'>
-        <h1 className='text-center'>Event List</h1>
 
-        <div className='row'>
-          <div className='col'>
-            <InfiniteScroll
-              style={{
-                MozColumnGap: '0.5em',
-                MozColumnWidth: '15em',
-                WebkitColumnGap: '0.5em',
-                WebkitColumnWidth: '15em'
-              }}
-              dataLength={this.state.events.length} // This is important field to render the next data
-              next={this.fetchEvents}
-              hasMore={!this.state.pageSettings.isLast}
-              loader={<h4>Loading...</h4>}
-              endMessage={
-                <p style={{ textAlign: 'center' }}>
-                  <b>Yay! You have seen it all</b>
-                </p>
-              }
-            >
-              {this.state.events.map((event, index) => (
-                <EventsListBuild {...event} key={index} />
-              ))}
-            </InfiniteScroll>
+        <h1 className='text-center'>Event List</h1>
+        <AddEventBtn />
+        <div className='container'>
+
+          <div className='row'>
+
+            {this.state.events.map((event, index) =>
+              (<EventsListBuild {...event} key={index} />)
+
+            )}
           </div>
         </div>
       </div>
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { CSSProperties } from 'react';
 import { RouterProps } from 'react-router';
