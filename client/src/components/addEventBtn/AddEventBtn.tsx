1c4df5f68e464839b4ec1dde6404d0400828aa34
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-03-26 14:38:16 +0200

    Added redirect to create-event

--- client/src/components/addEventBtn/AddEventBtn.tsx
+++ client/src/components/addEventBtn/AddEventBtn.tsx
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa';
 import "./AddEventBtn.scss"
-import { RouteComponentProps } from 'react-router';
+import { Redirect } from 'react-router';
 
 
 
 export class AddEventBtn extends Component<any, any> {
 
-    setRedirect = () => {
-        // this.props.history.push('/');
-        console.debug(this.props)
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            redirect: false
+        };
     }
 
+    public redirectTo() {
+        this.setState({ redirect: true });
+    }
     render() {
         return (
             <div className="btn-add ">
-                <button type="button" className="btn btn-info btn-circle btn-xl fixed-bottom " onClick={this.setRedirect}><FaPlus />
-
+                <button type="button" className="btn btn-info btn-circle btn-xl fixed-bottom " onClick={this.redirectTo.bind(this)}><FaPlus />
+                    {this.state.redirect ? (
+                        <Redirect
+                            push
+                            to={{
+                                pathname: `/create-event`,
+                                state: {
+                                    ...this.props
+                                }
+                            }}
+                        />
+                    ) : null}
                 </button>
             </div>)
     }
