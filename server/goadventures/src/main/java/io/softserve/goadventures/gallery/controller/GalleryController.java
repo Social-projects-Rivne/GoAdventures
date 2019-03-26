a6c3bc2bf9f5a9c7ab80a611f383a1d0726bbe30
Chubasic <joebereza@gmail.com>
2019-03-27 01:19:01 +0200

    Removed unused imports

--- server/goadventures/src/main/java/io/softserve/goadventures/gallery/controller/GalleryController.java
+++ server/goadventures/src/main/java/io/softserve/goadventures/gallery/controller/GalleryController.java
@@ -1,18 +1,15 @@
 package io.softserve.goadventures.gallery.controller;
 
+import java.util.HashSet;
+import java.util.Set;
 
-import io.softserve.goadventures.event.model.Event;
-import io.softserve.goadventures.event.service.EventService;
-import io.softserve.goadventures.gallery.model.Gallery;
-import io.softserve.goadventures.gallery.repository.GalleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import io.softserve.goadventures.event.service.EventService;
+import io.softserve.goadventures.gallery.model.Gallery;
+import io.softserve.goadventures.gallery.repository.GalleryRepository;
 
 @CrossOrigin
 @RestController
@@ -26,12 +23,13 @@ public class GalleryController {
 
   @PostMapping("/add-new")
   public ResponseEntity<?> addNewGallery() {
+    // creates mock gallery
     Event event = eventService.getEventById(8);
     Set<String> imageUrls = new HashSet<>();
-    imageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvT" +
-            "_LyIYyHGxrvoSKj4FteNg6o0LQYB-8f1gY1Cjd7jhvFyTGJpg");
-    imageUrls.add("https://2static1.fjcdn.com/thumbnails/comments/For+" +
-            "those+who+want+it+_f5bc15a12dc73ef31d8adb5357552a60.jpg");
+    imageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvT"
+        + "_LyIYyHGxrvoSKj4FteNg6o0LQYB-8f1gY1Cjd7jhvFyTGJpg");
+    imageUrls.add("https://2static1.fjcdn.com/thumbnails/comments/For+"
+        + "those+who+want+it+_f5bc15a12dc73ef31d8adb5357552a60.jpg");
     Gallery mockGallery = new Gallery(0, event, imageUrls, false);
     galleryRepository.save(mockGallery);
     return ResponseEntity.ok().body(mockGallery);
