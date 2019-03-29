85d8a4767e5ae4bbe1ba7053b5e169b0487af7e1
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-03-29 13:42:35 +0200

    fixed card columns

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -4,6 +4,7 @@ import { getEventList } from '../../api/event.service';
 import { AddEventBtn } from '../../components/addEventBtn/AddEventBtn';
 import { EventsListBuild } from '../../components/eventsListBuild/EventsListBuild';
 import { EventDto } from '../../interfaces/Event.dto';
+import './Events.scss';
 
 interface EventState {
   events: EventDto[];
@@ -63,32 +64,30 @@ export class Events extends Component<EventDto, EventState> {
       <div className='container-fluid'>
         <h1 className='text-center'>Event List</h1>
         <AddEventBtn />
-        <div className='row'>
-          <div className='col'>
-            <InfiniteScroll
-              style={{
-                MozColumnGap: '0.5em',
-                MozColumnWidth: '15em',
-                WebkitColumnGap: '0.5em',
-                WebkitColumnWidth: '15em'
-              }}
-              dataLength={this.state.events.length} // This is important field to render the next data
-              next={this.fetchEvents}
-              hasMore={!this.state.pageSettings.isLast}
-              loader={<h4>Loading...</h4>}
-              endMessage={
-                <p style={{ textAlign: 'center' }}>
-                  <b>Yay! You have seen it all</b>
-                </p>
-              }
-            >
+
+        <InfiniteScroll
+
+          dataLength={this.state.events.length} // This is important field to render the next data
+          next={this.fetchEvents}
+          hasMore={!this.state.pageSettings.isLast}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <div className='container'>
+            <div className='card-columns'>
               {this.state.events.map((event, index) => (
                 <EventsListBuild {...event} key={index} />
               ))}
-            </InfiniteScroll>
+            </div>
           </div>
-        </div>
+        </InfiniteScroll>
+
       </div>
+
     );
   }
 }
