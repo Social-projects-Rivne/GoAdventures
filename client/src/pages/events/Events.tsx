a5c76448e5e9f1bcddc0ff92a959bc50b161118e
Artem Korniychuk <artem17thomas@gmail.com>
2019-04-04 14:44:12 +0300

    Added search to front and connection to api. Without styles.

--- client/src/pages/events/Events.tsx
+++ client/src/pages/events/Events.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import { getEventList } from '../../api/event.service';
+import { getEventList, searchForEvents } from '../../api/event.service';
 import { AddEventBtn } from '../../components/addEventBtn/AddEventBtn';
 import { EventsListBuild } from '../../components/eventsListBuild/EventsListBuild';
 import { EventDto } from '../../interfaces/Event.dto';
@@ -12,6 +12,7 @@ interface EventState {
     isLast: true | false | undefined
     nextPage: string | null
   };
+  search: string | null | undefined;
 }
 
 export class Events extends Component<EventDto, EventState> {
@@ -22,17 +23,47 @@ export class Events extends Component<EventDto, EventState> {
       pageSettings: {
         isLast: undefined,
         nextPage: ''
-      }
+      },
+      search: ''
     };
     this.fetchEvents = this.fetchEvents.bind(this);
+  }
+  handleChange(value: string) {
+    if (window.location.pathname != '/events') {
+      // <Link ></Link>
+
+    }
+    this.setState({ search: value });
   }
 
   public componentDidMount() {
     this.fetchEvents();
   }
 
+  public async fetchSearchEvent(): Promise<void> {
+    this.state = {
+      events: [],
+      pageSettings: {
+        isLast: undefined,
+        nextPage: ''
+      },
+      search: this.state.search
+    };
+    const response = await searchForEvents(null, this.state.search);
+    this.setState({
+      events: [...this.state.events, ...response.content],
+      pageSettings: {
+        isLast: response.last,
+        nextPage: !!sessionStorage.getItem('nextpage')
+          ? sessionStorage.getItem('nextpage')
+          : '/event/all'
+      }
+    });
+  }
+
   public async fetchEvents(): Promise<void> {
-    console.debug(this.state);
+
+
     const response = await getEventList(this.state.pageSettings.nextPage);
     this.setState({
       events: [...this.state.events, ...response.content],
@@ -49,6 +80,14 @@ export class Events extends Component<EventDto, EventState> {
     return (
       <div className='container-fluid'>
         <h1 className='text-center'>Event List</h1>
+        <form onSubmit={(e: any) => {
+          e.preventDefault();
+          this.fetchSearchEvent();
+        }}>
+          <input placeholder="search" type='' onChange={(e: any) => {
+            this.handleChange(e.target.value);
+          }} />
+        </form>
         <AddEventBtn />
         <div className='row'>
           <div className='col'>
