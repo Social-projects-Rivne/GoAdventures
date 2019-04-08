3fe0b55960d80fbb0fefb4327d56336217c677f2
= <kobukstr@gmail.com>
2019-04-08 13:29:58 +0300

    Merge develop in feature/close-reopen

--- client/src/pages/events/EventsDetail/EventDetail.tsx
+++ client/src/pages/events/EventsDetail/EventDetail.tsx
@@ -102,9 +102,6 @@ export class EventDetail extends Component<any, any> {
           </div>
           <div className='col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5'>
             <div className='jumboton jumbotron-fluid'>
-              {this.state.eventProps.event.statusId === 2 ? (
-                 <p style = {{color:'red'}}>CLOSED</p>
-               ) : null}
               <SettingsPanel>
                 {{
                   left: (
@@ -215,7 +212,9 @@ export class EventDetail extends Component<any, any> {
               ) : (
                 <div />
               )}
-
+              {this.state.eventProps.event.statusId === 2 ? (
+                 <p style = {{color:'red'}}>CLOSED</p>
+               ) : null}
               <hr className='my-4' />
               <div>
                 <h2>Comments</h2>
