1c3a99d5196f0cac41915471d4f7b5b2e90149bd
Ivan Tymoschuk <ivantymo99@gmail.com>
2019-04-10 13:53:42 +0300

    Redesigned: Event list page, event detail

--- client/src/pages/events/EventsDetail/EventDetail.tsx
+++ client/src/pages/events/EventsDetail/EventDetail.tsx
@@ -2,10 +2,10 @@ import { AxiosResponse } from 'axios';
 import React, { Component, SyntheticEvent } from 'react';
 import { Field, FieldProps, Form, Formik, FormikProps } from 'formik';
 import { TileLayer, Map, Marker, Popup } from 'react-leaflet';
-import { MdDone } from 'react-icons/md';
+import { MdDone, MdLockOpen, MdEdit, MdDelete, MdLock } from 'react-icons/md';
 import moment from 'moment';
 import { deleteEvent, isOwner, closeEvent, openEvent } from '../../../api/event.service';
-import { Comments, Gallery, SettingsPanel } from '../../../components';
+import { Comments, Gallery } from '../../../components';
 import { commentsSchema } from '../../../validationSchemas/commentValidation';
 import './EventDetail.scss';
 
@@ -95,62 +95,107 @@ export class EventDetail extends Component<any, any> {
     const style = this.state.eventProps.event.statusId === 2 ? { display: 'none' } : {};
 
     return (
-      <div className='container EventDetail'>
+      <div className='container page-container EventDetail'>
         <div className='row'>
           <div className='col-12 col-sm-12 col-md-6 col-lg-7 col-xl-7'>
-            <Gallery {...this.state.eventProps.event.gallery} />
+            <Gallery class="gallery" {...this.state.eventProps.event.gallery} />
           </div>
           <div className='col-12 col-sm-12 col-md-6 col-lg-5 col-xl-5'>
             <div className='jumboton jumbotron-fluid'>
-              <SettingsPanel>
-                {{
-                  left: (
-                    <div>
-                      <div className='d-flex flex-row align-content-center mt-3'>
-                        <img
-                          className='rounded-avatar-sm'
-                          src='https://www.kidzone.ws/animal-facts/whales/images/beluga-whale-3.jpg'
-                        />
-                        <h2>{this.state.eventProps.event.topic}</h2>
-                      </div>
-                      <p>
-                        Start:
+              <div className='d-flex flex-row align-content-center mt-3'>
+                <img
+                  className='rounded-avatar-sm'
+                  src='https://www.kidzone.ws/animal-facts/whales/images/beluga-whale-3.jpg'
+                />
+                <h2>{this.state.eventProps.event.topic}</h2>
+              </div>
+              <div className="SettingsPanel">
+                <div className='content-column d-flex flex-column h-100'>
+
+                  <p>
+                    Start:
                         {this.convertTime(
-                          this.state.eventProps.event.startDate.toString()
-                        )}
-                      </p>
-                      <p>
-                        Ends:
+                      this.state.eventProps.event.startDate.toString()
+                    )}
+                  </p>
+                  <p>
+                    Ends:
                         {this.state.eventProps.event.endDate === '0' ? 'Push edit if you want to change end date' : (
-                          this.convertTime(
-                          this.state.eventProps.event.endDate.toString()
-                        )
-                        )}
-                      </p>
-                    </div>
-                  ),                    
-                  right: (
-                    <button
-                      type='button'
-                      className='btn btn-outline-info btn-sm'
-                      style={style}
-                    >
-                      Subscribe
+                      this.convertTime(
+                        this.state.eventProps.event.endDate.toString()
+                      )
+                    )}
+                  </p>
+                </div>
+
+                <div className='content-column d-flex flex-column h-100'>
+                  <button
+                    type='button'
+                    className='btn btn-outline-info btn-sm'
+                    style={style}
+                  >
+                    Subscribe
                     </button>
-                  )
-                }}
-              </SettingsPanel>
+
+                  {this.state.isOwner ? (
+                    <div>
+                      <button
+                        onClick={this.handleDelete}
+                        type='button'
+                        className='btn btn-lg btn-outline-danger'
+                      >
+                        <MdDelete></MdDelete>
+                      </button>
+                      <button
+                        onClick={() => {
+                          this.state.eventProps.setEdit(true);
+                        }}
+                        type='button'
+                        className='btn btn-lg btn-outline-success'
+                      >
+                        <MdEdit></MdEdit>
+                      </button>
+                      {this.state.eventProps.event.statusId === 2 ? (
+                        <button
+                          onClick={this.handleOpen}
+                          type='button'
+                          className='btn btn-lg btn-outline-warning'
+                          
+                        >
+                          <MdLockOpen></MdLockOpen>
+                        </button>
+                      )
+                        : (
+                          <button
+                            onClick={this.handleClose}
+                            type='button'
+                            className='btn btn-lg btn-outline-warning'
+                            
+                          >
+                            <MdLock></MdLock>
+                      </button>
+                        )}
+                    </div>
+                  ) : (
+                      <div />
+                    )}
+                  {this.state.eventProps.event.statusId === 2 ? (
+                    <p style={{ color: 'red' }}>CLOSED</p>
+                  ) : null}
+
+                </div>
+              </div>
               <hr className='my-4' />
               <span className='lead'>
                 {this.state.eventProps.event.description}
               </span>
               <hr className='my-4' />
               <div className='map'>
-                <h2>Location and Destination points</h2>
+                <h3>Location and Destination points</h3>
                 <div className='rounded'>
                   <Map
-                  zoomControl={false}
-                    className='rounded'
+                    zoomControl={false}
+                    className='rounded map-layer'
                     center={[
                       this.state.eventProps.event.latitude,
                       this.state.eventProps.event.longitude
@@ -172,52 +217,10 @@ export class EventDetail extends Component<any, any> {
                 </div>
                 {this.state.eventProps.event.location}
               </div>
-              {this.state.isOwner ? (
-                <div>
-                  <button
-                    onClick={this.handleDelete}
-                    type='button'
-                    className='btn btn-danger'
-                  >
-                    Delete
-                  </button>
-                  <button
-                    onClick={() => {
-                      this.state.eventProps.setEdit(true);
-                    }}
-                    type='button'
-                    className='btn btn-success'
-                  >
-                    Edit
-                  </button>
-                  {this.state.eventProps.event.statusId === 2 ? (
-                    <button
-                  onClick={this.handleOpen}
-                  type='button'
-                  className='btn btn-warning'
-                >
-                  Open
-                </button>
-                  )
-                  : (
-                    <button
-                    onClick={this.handleClose}
-                    type='button'
-                    className='btn btn-warning'
-                  >
-                    Close
-                  </button>
-                  )}
-                </div>
-              ) : (
-                <div />
-              )}
-              {this.state.eventProps.event.statusId === 2 ? (
-                 <p style = {{color:'red'}}>CLOSED</p>
-               ) : null}
+
               <hr className='my-4' />
               <div>
-                <h2>Comments</h2>
+                <h3>Comments</h3>
                 <div>
                   <Formik
                     validationSchema={commentsSchema}
@@ -225,7 +228,7 @@ export class EventDetail extends Component<any, any> {
                     enableReinitialize={true}
                     validateOnBlur={true}
                     validateOnChange={true}
-                    onSubmit={() => {}}
+                    onSubmit={() => { }}
                     render={(props: FormikProps<FormValue>) => (
                       <Form>
                         <Field
@@ -241,16 +244,16 @@ export class EventDetail extends Component<any, any> {
                                   name='comment'
                                 />
                                 {form.touched.comment &&
-                                form.errors.comment &&
-                                form.errors.comment ? (
-                                  <div className='invalid-feedback'>
-                                    {form.errors.comment}
+                                  form.errors.comment &&
+                                  form.errors.comment ? (
+                                    <div className='invalid-feedback'>
+                                      {form.errors.comment}
+                                    </div>
+                                  ) : (
+                                    <div className='valid-feedback'>
+                                      <MdDone /> Press enter to add comment
                                   </div>
-                                ) : (
-                                  <div className='valid-feedback'>
-                                    <MdDone /> Press enter to add comment
-                                  </div>
-                                )}
+                                  )}
                               </div>
                             );
                           }}
