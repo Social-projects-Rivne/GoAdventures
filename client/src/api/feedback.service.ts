6a3827d9935e96ba296a7bc9070e7f404397ca13
Chubasic <joebereza@gmail.com>
2019-04-16 14:23:12 +0300

    feedback requests

---
+++ client/src/api/feedback.service.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { serverUrl } from './url.config';
+import { cookies } from './cookies.service';
+import errorHandle from './error.service';
+import { AxiosResponse, AxiosError } from 'axios';
+import { FeedbackDTO } from '../interfaces/Feedback.dto';
+
+export const getFeedback = async (eventId: number): Promise<any> => {
+  return await axios
+    .get(`${serverUrl}/get-feedback/${eventId}`, {
+      headers: {
+        Authorization: `Bearer ${cookies.get('tk879n')}`
+      }
+    })
+    .then((res: AxiosResponse<any>) => {
+      return res.data;
+    })
+    .catch((err: AxiosError) => {
+      console.debug(err);
+      return errorHandle(err);
+    });
+};
+
+export const addFeedback = async (
+  eventId: number,
+  feedback: FeedbackDTO
+): Promise<any> => {
+  return await axios.post(`${serverUrl}/add-feedback/${eventId}`, feedback, {
+    headers: {
+      Authorization: `Bearer ${cookies.get('tk879n')}`
+    }
+  });
+};
